3427a8476c80721d45ef11317dbfacc1cffb1502
Frandy Slueue <frandy4ever@gmail.com>
2024-09-08 03:49:22 -0500

    Concatenate the values of two string variables

---
+++ Python/python-hello_world/4-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# A scritp that prints the concatenation of
+# str1 and str2 with exactly 5 lines long code
+
+str1 = "Holberton"
+str2 = "School"
+
+print(f'{str1} {str2}')
