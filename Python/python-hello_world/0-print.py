499f5184ced94f6255919c5de86e54691235a4ea
Frandy Slueue <frandy4ever@gmail.com>
2024-09-08 00:57:10 -0500

    Initial commit

---
+++ Python/python-hello_world/0-print.py
@@ -0,0 +1,5 @@
+# A script that prints exactly 
+# "Programming is like building a multilingual puzzle, 
+# followed by a new line using the print function.
+
+print('"Programming is like building a multilingual puzzle\n')
